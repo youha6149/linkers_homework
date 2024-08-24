1bb4b0e86d5e198935df784cf2530071a5fb8d9c
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-08-24 20:11:45 +0900

    Use index.py and utils.py to main

--- main.py
+++ main.py
@@ -7,16 +7,13 @@ from utils import create_2gram, display_results, normalize_key
 def main():
     csv_file_path = "./address/zenkoku.csv"
     inverted_index_file = "inverted_index.pkl"
-
-    # 転置インデックスの作成と保存
+    inverted_index_manager = InvertedIndexManager()
     try:
+
         with open(csv_file_path, "r", encoding="shift-jis", errors="ignore") as file:
             reader = csv.DictReader(file)
-            inverted_index = build_inverted_index(reader)
-            normalized_index = {
-                normalize_key(key): value for key, value in inverted_index.items()
-            }
-            save_inverted_index(inverted_index_file, normalized_index)
+            inverted_index_manager.build(reader)
+            inverted_index_manager.save(inverted_index_file)
 
     except FileNotFoundError:
         print(f"Error: CSVファイルが見つかりません: {csv_file_path}")
@@ -28,10 +25,15 @@ def main():
 
     # 検索処理
     query = input("検索したい地名や住所の一部を入力してください: ")
-    inverted_index = load_inverted_index(inverted_index_file)
-    matching_lines = search_inverted_index(inverted_index, query)
+    query_params = [normalize_key(q) for q in create_2gram(query)]
 
-    display_results(csv_file_path, matching_lines)
+    inverted_index_manager.load(inverted_index_file)
+    matching_lines = inverted_index_manager.search(query_params)
+
+    with open(csv_file_path, "r", encoding="shift-jis", errors="ignore") as file:
+        reader = csv.DictReader(file)
+        display_results(reader, matching_lines)
+
     print(f"検索結果: {len(matching_lines)} 件")
 
 
