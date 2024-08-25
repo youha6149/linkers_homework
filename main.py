4d1dec2e81ba84942e00b97e7b563103749a9a0a
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-08-25 14:50:45 +0900

    Fix main to create and call a separate func for creating and searching the inverted index

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from indexing import InvertedIndexManager
 from utils import display_results
 
 
-def main():
-    csv_file_path = "./address/zenkoku.csv"
-    inverted_index_file = "./db/inverted_index.pkl"
+def create_inverted_index(
+    csv_file_path: str = "./address/zenkoku.csv",
+    inverted_index_file: str = "./db/inverted_index.pkl",
+) -> None:
+    """転置インデックスを作成してファイルに保存する"""
     inverted_index_manager = InvertedIndexManager()
     try:
-
         with open(csv_file_path, "r", encoding="shift-jis", errors="ignore") as file:
             reader = csv.DictReader(file)
             inverted_index_manager.build(reader)
@@ -20,13 +21,25 @@ def main():
         return
 
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error: 転置インデックスの作成中にエラーが発生しました: {e}")
         return
 
-    # 検索処理
-    query = input("検索したい地名や住所の一部を入力してください: ")
 
-    inverted_index_manager.load(inverted_index_file)
+def search_inverted_index(
+    csv_file_path: str = "./address/zenkoku.csv",
+    inverted_index_file: str = "./db/inverted_index.pkl",
+) -> None:
+    """転置インデックスを使用して検索を行う"""
+    query = input("検索したい地名や住所の一部を入力してください: ")
+    inverted_index_manager = InvertedIndexManager()
+
+    try:
+        inverted_index_manager.load(inverted_index_file)
+
+    except Exception as e:
+        print(f"Error: 転置インデックスの読み込み中にエラーが発生しました: {e}")
+        return
+
     matching_lines = inverted_index_manager.search(query)
 
     with open(csv_file_path, "r", encoding="shift-jis", errors="ignore") as file:
@@ -34,6 +47,15 @@ def main():
         display_results(reader, matching_lines)
 
     print(f"検索結果: {len(matching_lines)} 件")
+
+
+def main():
+
+    # 転置インデックスの作成
+    create_inverted_index()
+
+    # 検索処理
+    search_inverted_index()
 
 
 if __name__ == "__main__":
